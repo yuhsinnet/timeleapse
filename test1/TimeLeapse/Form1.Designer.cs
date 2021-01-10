e862edc858dc1232c981d0b5f3bdabe4ce1d3cab
YUHSIN <sg@cjee.com.tw>
2021-01-10 13:51:57 +0800

    取得錄影帶
    初步可用時間自動取得錄影帶資料

--- test1/TimeLeapse/Form1.Designer.cs
+++ test1/TimeLeapse/Form1.Designer.cs
@@ -28,13 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axGVSinglePlayer1 = new AxGVSINGLEPLAYERLib.AxGVSinglePlayer();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axGVSinglePlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axGVSinglePlayer1
+            // 
+            this.axGVSinglePlayer1.Enabled = true;
+            this.axGVSinglePlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axGVSinglePlayer1.Name = "axGVSinglePlayer1";
+            this.axGVSinglePlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axGVSinglePlayer1.OcxState")));
+            this.axGVSinglePlayer1.Size = new System.Drawing.Size(474, 426);
+            this.axGVSinglePlayer1.TabIndex = 0;
+            this.axGVSinglePlayer1.LoginOK += new System.EventHandler(this.axGVSinglePlayer1_LoginOK);
+            this.axGVSinglePlayer1.LoginEvent += new AxGVSINGLEPLAYERLib._DGVSinglePlayerEvents_LoginEventHandler(this.axGVSinglePlayer1_LoginEvent);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(498, 12);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy/MM/dd HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(498, 186);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(220, 22);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(553, 214);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(100, 22);
+            this.txtIP.TabIndex = 3;
+            this.txtIP.Text = "192.168.0.201";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(553, 242);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 22);
+            this.txtPort.TabIndex = 4;
+            this.txtPort.Text = "5552";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(553, 270);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 22);
+            this.txtUser.TabIndex = 5;
+            this.txtUser.Text = "cj";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(553, 298);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(100, 22);
+            this.txtPwd.TabIndex = 6;
+            this.txtPwd.Text = "54";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(529, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(520, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(518, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "User:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(492, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "PassWord:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(496, 426);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "label5";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Get Snapshot";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(573, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Get Bitmap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.axGVSinglePlayer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axGVSinglePlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private AxGVSINGLEPLAYERLib.AxGVSinglePlayer axGVSinglePlayer1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- test1/TimeLeapse/Form1.cs
+++ test1/TimeLeapse/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TimeLeapse
@@ -15,6 +8,88 @@ namespace TimeLeapse
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+
+
+
+            dateTimePicker1.Value = e.Start;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axGVSinglePlayer1.ShowTreeList = true;
+
+
+            axGVSinglePlayer1.Login2(txtIP.Text,
+                                     int.Parse(txtPort.Text),
+                                     txtUser.Text,
+                                     txtPwd.Text);//   'Start login process
+
+        }
+
+        private void axGVSinglePlayer1_LoginOK(object sender, EventArgs e)
+        {
+            label5.Text = "Login OK!";
+        }
+       
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            int HostType = 0;
+            int nDBType = 0; //Default value is 0. Reserved.
+            int nCamera = 2;
+            string lpDateTime = dateTimePicker1.Value.ToString("yyyyMMddHHmmss") + "002";
+
+            axGVSinglePlayer1.SearchAndPlay(HostType, nDBType, nCamera, lpDateTime);
+        }
+
+        private void axGVSinglePlayer1_LoginEvent(object sender, AxGVSINGLEPLAYERLib._DGVSinglePlayerEvents_LoginEvent e)
+        {
+            string iStatus = "";
+            switch (e.iStatus)
+            {
+
+                case 0:
+                    iStatus = "Login successful.";
+                    break;
+                case 1:
+                    iStatus = "Failed to connect to the server.";
+                    break;
+                case 2:
+                    iStatus = "Login failed.";
+                    break;
+                case 3:
+                    iStatus = "Exceed the maximum connection of the server.";
+                    break;
+                case 4:
+                    iStatus = "The application is too old.";
+                    break;
+                case 5:
+                    iStatus = "The CODEC of the application is too old.";
+                    break;
+
+
+                default:
+                    break;
+            }
+
+
+            label5.Text = iStatus;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axGVSinglePlayer1.SnapShot(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\image.jpg");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axGVSinglePlayer1.GetBitmap(true, Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\image.bmp");
         }
     }
 }
